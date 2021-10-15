0d186279e470cfbfbba0e638462d0f13eee2e531
Thomas CANU <t.canu@formation-cci12.fr>
2021-10-15 16:59:35 +0200

    add hidden

--- Le_Labo/Accueil_Page/jvc.js
+++ Le_Labo/Accueil_Page/jvc.js
@@ -1,15 +1,66 @@
-var date = new Date();
- var heure =date.getHours();
- var minute=date.getMinutes();
- var seconde=date.getSeconds();
-var f = function() {
-if(seconde<59)
-seconde++;
-else
-{minute++;seconde=00;}
-if(minute>59)
-{heure++;minute=0;}
-document.getElementById("horloge").textContent=heure+":"+minute+":"+seconde;
- setTimeout(ff, 1000);
+var imgIndex = 0;
+
+function initGalery() {
+	var photos = document.getElementById('galerie_mini');
+  	var liens = photos.getElementsByTagName('img');
+	var text = document.getElementById('text-img');
+	var big_photo = document.getElementById('big_pict');
+	big_photo.src = liens[imgIndex].src; // On change l'attribut src de l'image en le remplaçant par la valeur du lien
+	big_photo.title = liens[imgIndex].title;
+	text.innerHTML = ((imgIndex+1) + '/' + liens.length);
+
+	var main = document.getElementById('main');
+	var hidden = document.getElementById('playerJS');
+	main.innerHTML = hidden.innerHTML;
+
 }
- setTimeout(f, 1000);
+
+function loadDoc() {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById('galerie_mini').innerHTML = this.responseText;
+    }
+  };
+  xhttp.open("GET", "data2.xml", true);
+  xhttp.send();
+
+}
+
+function forwardGalery() {
+	var photos = document.getElementById('galerie_mini');
+  	var liens = photos.getElementsByTagName('img');
+	var text = document.getElementById('text-img');
+	var big_photo = document.getElementById('big_pict');
+	if(imgIndex == liens.length-1)
+	{
+		imgIndex = 0;
+	}
+	else
+	{
+		imgIndex++;
+	}
+	big_photo.src = liens[imgIndex].src; // On change l'attribut src de l'image en le remplaçant par la valeur du lien
+	big_photo.title = liens[imgIndex].title;
+	text.innerHTML = ((imgIndex+1) + '/' + liens.length);
+}
+
+function backwardGalery() {
+	var photos = document.getElementById('galerie_mini');
+  	var liens = photos.getElementsByTagName('img');
+	var text = document.getElementById('text-img');
+	var big_photo = document.getElementById('big_pict');
+	if(imgIndex == 0)
+	{
+		imgIndex = liens.length-1;
+	}
+	else
+	{
+		imgIndex--;
+	}
+	big_photo.src = liens[imgIndex].src; // On change l'attribut src de l'image en le remplaçant par la valeur du lien
+	big_photo.title = liens[imgIndex].title;
+	text.innerHTML = ((imgIndex+1) + '/' + liens.length);
+}
+
+loadDoc();
